36f9209f6135c926b49c4520331cf5d0bb081e71
Shinnosuke612 <K024G0100@g.neec.ac.jp>
2025-11-19 16:06:54 +0900

    WindowAPIをクラス化した

---
+++ project/WinApp.h
@@ -0,0 +1,35 @@
+#pragma once
+#include <Windows.h>
+#include <cstdint>
+class WinApp{
+public:
+
+	//静的メンバ関数
+	static LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam);
+
+	//初期化
+	void Initialize();
+	//終了
+	void Finalize();
+	//メッセージの処理
+	bool ProcessMessage();
+
+	//クライアント領域のサイズ
+	static const int32_t kClientWidth = 1280;
+	static const int32_t kClientHeight = 720;
+
+	//getter
+	HWND GetHwnd() const{
+		return hwnd;
+	}
+	HINSTANCE GetHInstance() const{
+		return wc.hInstance;
+	}
+
+private:
+	//ウィンドウハンドル
+	HWND hwnd = nullptr;
+	//ウィンドウクラスの設定
+	WNDCLASS wc{};
+};
+
