4c6ecdeb8af1bf5f53e76e4896b31bc30d162d00
Shinnosuke612 <K024G0100@g.neec.ac.jp>
2025-12-05 11:42:52 +0900

    60FPSに固定した

--- project/DirectXCommon.cpp
+++ project/DirectXCommon.cpp
@@ -4,6 +4,7 @@
 #pragma comment(lib,"d3d12.lib")
 #pragma comment(lib,"dxgi.lib")
 #include <format>
+#include <thread>
 
 using namespace Microsoft::WRL;
 using namespace Logger;
@@ -14,6 +15,8 @@ void DirectXCommon::Initialize(WinApp* winApp){
 	//メンバ変数に記録
 	winApp_ = winApp;
 	
+	//FPS固定初期化
+	InitializeFixFPS();
 	//デバイスの生成
 	DeviceInitialize();
 	//コマンド関連の初期化
@@ -135,6 +138,9 @@ void DirectXCommon::PostDraw(){
 	// GPUにコマンドリストの実行を行わせる
 	ID3D12CommandList* commandLists[] = { commandList.Get()};
 	commandQueue->ExecuteCommandLists(1, commandLists);
+
+	//FPS固定
+	UpdateFixFPS();
 
 	// GPUとOSに画面の交換を行うよう通知する
 	swapChain->Present(1, 0);
@@ -634,6 +640,37 @@ D3D12_GPU_DESCRIPTOR_HANDLE DirectXCommon::GetGPUDescriptorHandle(const Microsof
 	D3D12_GPU_DESCRIPTOR_HANDLE handleGPU = descriptorHeap->GetGPUDescriptorHandleForHeapStart();
 	handleGPU.ptr += (descriptorSize * index);
 	return handleGPU;
+
+}
+
+void DirectXCommon::InitializeFixFPS(){
+	//現在史観を記録する
+	reference_ = std::chrono::steady_clock::now();
+
+}
+
+void DirectXCommon::UpdateFixFPS(){
+	//1/60秒ぴったりの時間
+	const std::chrono::microseconds kMinTime(uint64_t(1000000.0f / 60.0f));
+	//1/60秒よりわずかに短い時間
+	const std::chrono::microseconds kMinCheckTime(uint64_t(1000000.0f / 65.0f));
+
+	//現在時間を取得する
+	std::chrono::steady_clock::time_point now = std::chrono::steady_clock::now();
+	//前回記録からの経過時間を取得する
+	std::chrono::microseconds elapsed =
+		std::chrono::duration_cast<std::chrono::microseconds>(now - reference_);
+
+	//1/60秒(よりわずかに短い時間)経っていない場合
+	if(elapsed < kMinCheckTime){
+		//1/60秒経過するまで微小なスリープを繰り返す
+		while(std::chrono::steady_clock::now() - reference_ < kMinTime){
+			//1マイクロ秒スリープ
+			std::this_thread::sleep_for(std::chrono::microseconds(1));
+		}
+	}
+	//現在時間を記録する
+	reference_ = std::chrono::steady_clock::now();
 
 }
 
